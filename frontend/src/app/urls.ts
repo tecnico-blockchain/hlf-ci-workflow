b6c03054878b40b2938fec85229c459f955f9b46
André Breda <andrebreda@tecnico.ulisboa.pt>
2024-12-02 16:54:51 +0000

    frontend: fix

--- frontend/src/app/urls.ts
+++ frontend/src/app/urls.ts
@@ -1,6 +1,5 @@
 export class URLS {
-    // private static IP = "http://localhost:8080/";
-    private static IP = process.env['API_ENDPOINT'] || "https://restapi.localho.st/";
+    private static IP = "http://rest-api.localho.st/";
     public static LIST = URLS.IP + "list";
     public static CREATE = URLS.IP + "create";
     public static UPDATE = URLS.IP + "update";
